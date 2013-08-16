e028ff7e85201dd9f91475b5579ed51e81a3d6ab
alex <ahernandezsis@unm.edu>
2013-08-16 11:36:58 -0600

    Added HelloWorld class and main testing class
    Signed-off-by: alex <ahernandezsis@unm.edu>

---
+++ Demo/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	
+	
+	
+	public void sayHello (){
+		
+		System.out.println("Hello World");
+		
+	}
+	
+
+}
---
+++ Demo/src/TestHello.java
@@ -0,0 +1,14 @@
+
+public class TestHello {
+
+	public static void main(String[] args) {
+
+		
+		HelloWorld hw = new HelloWorld();
+		
+		hw.sayHello();
+		
+		
+	}
+
+}
